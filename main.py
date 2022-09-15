ff6be7fa4dd793f0c4c4a38cc076628a303ad6e5
unknown <danielclichotti@gmail.com>
2022-09-14 23:01:03 -0300

    Comitando o codigo comentado

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from sqlalchemy import create_engine
 import pymysql
 import pandas as pd
 
-user = 'root'
-password = 'toor'
-host = '127.0.0.1'
-port = 3306
-database = 'statlog'
+user = 'root' #usuario mySQL
+password = 'toor' #senha mySQL
+host = '127.0.0.1' #host
+port = 3306 #porta
+database = 'statlog' #nome do database
 
-sqlEngine = create_engine(url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(user, password, host, port, database))
+sqlEngine = create_engine(url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(user, password, host, port, database)) #conectando ao bd
 
-dbConnection = sqlEngine.connect()
+dbConnection = sqlEngine.connect() #conectando
 
-creditoDataSet = pd.read_sql("select * from statlog.germancredit", dbConnection);
+creditoDataSet = pd.read_sql("select * from statlog.germancredit", dbConnection) #salvando o dataset
 
 pd.set_option('display.expand_frame_repr', False)
 
+#mudar a partir daqui usando a ia
 treinoDataSet = creditoDataSet[0:int(creditoDataSet.shape[0] * 0.8)]
 avaliacDataSet = creditoDataSet[int(creditoDataSet.shape[0] * 0.8):]
 
@@ -27,4 +28,4 @@ print(avaliacDataSet.shape)
 
 
 
-dbConnection.close()
+dbConnection.close() #fechando a conex
