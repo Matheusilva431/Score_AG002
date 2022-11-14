ee4c7fd454c89209ba99061f30c63ad9c199af54
Matheus Chagas <matheusilva431+github@gmail.com>
2022-11-13 21:30:40 -0300

    Ajustando import e requirements

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
 from perceptron import Perceptron
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.neural_network import MLPClassifier
